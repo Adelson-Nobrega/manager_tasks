01121f243d155283e15deeb2ba9de5c024941ab6
Teumr8-UX <teteumr8@hotmail.com>
2026-01-25 10:40:24 -0300

    Incluindo atributos e método constutor da classe Tarefa.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public static void main(String[] args) {
 
     System.out.println("Testando o GitHub");
 
-    Tarefa tarefa = new Tarefa();
-    tarefa.executar();
+
 }
 }
--- src/Tarefa.java
+++ src/Tarefa.java
@@ -1,6 +1,25 @@
+import java.time.LocalDate;
+
 public class Tarefa {
 
-  public void executar (){
+        private String titulo;
+        private String descricao;
+        private LocalDate dataLimite;
+
+
+        public Tarefa (String titulo, String descricao, LocalDate dataLimite){
+            this.titulo = titulo;
+            this.descricao = descricao;
+            this.dataLimite = dataLimite;
+
     System.out.println("Testando o comando pull");
-  }
-}
+
+            }
+        @Override
+        public String toString (){
+            return "Título: " + titulo +
+                    "\nDescrição: " + descricao +
+                    "\nData Limite: " + dataLimite +
+                    "\n-------------------------";
+        }
+    }
