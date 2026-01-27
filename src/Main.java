0e1220fad1cd55c7d693e41879062a64639764f4
Teumr8-UX <suehtam_8@hotmail.com>
2026-01-27 08:37:38 -0300

    Incluindo atributos e método constutor da classe Tarefa.

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,15 @@ public static void main(String[] args) {
     System.out.println("Testando o GitHub");
     System.out.println("Hello World");
 
+//Menu do Sistema
+    System.out.println("\n1 - Cadastrar Tarefa");
+    System.out.println("2 - Listar Tarefas");
+    System.out.println("3 - Filtrar por Status");
+    System.out.println("4 - Listar por Data Limite");
+    System.out.println("0 - Sair");
+
+
+
+
 }
 }
